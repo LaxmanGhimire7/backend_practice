04fd171522bcc8891e95251f9c85172dfa7658c7
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-01-30 18:44:21 +0545

    backend structure done up to connect to DB

---
+++ practice-4/server.js
@@ -0,0 +1,11 @@
+// server start garna and database connect garna
+
+require("dotenv").config()
+const app = require('./src/app');
+
+const connectToDb = require('./src/config/database')
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000....")
+})
