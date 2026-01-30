9ccc2219186f498bcc43a58e5fe79b38a8dff130
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-01-30 19:13:24 +0545

    practice-4 POST, GET

--- practice-4/src/app.js
+++ practice-4/src/app.js
@@ -2,8 +2,34 @@
 
 const express = require("express");
 const app = express();
+const noteModel = require("./model/note.model");
+
+app.use(express.json());
 
 
+//Created API to create note => POST & saved in DB also
+app.post("/notes", async (req, res) => {
+  const { title, description } = req.body;
+
+  const notes = await noteModel.create({
+    title,
+    description,
+  });
+
+  res.status(201).json({
+    message: "Note created successfully",
+    notes,
+  });
+});
+
+
+// Creating API to get the data from theDB
+app.get("/notes",async(req,res)=>{
+    const notes = await noteModel.find();
+    res.status(200).json({
+        notes
+    })
+})
 
 
 module.exports = app;
