12cf01fcd06b9232000a537d7f33e462de6e7fc4
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-02-06 23:34:13 +0545

    get

--- practice-7/Backend/src/app.js
+++ practice-7/Backend/src/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const userModel = require("./models/user.model")
 
+app.use(express.json())
+
 app.post("/api/user",async(req,res)=>{
 
     const {name, email, password} = req.body;
@@ -11,6 +13,15 @@ app.post("/api/user",async(req,res)=>{
 
     res.status(201).json({
         message:"User created successfully...",
+        user
+    })
+})
+
+app.get("/api/user",async(req,res)=>{
+    
+   const user = await userModel.find()
+    res.status(200).json({
+        message:"User fetched successfully..",
         user
     })
 })
