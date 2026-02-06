75ab3341d19e1643be1121574a789860e1897109
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-02-06 23:52:03 +0545

    crud api

--- practice-7/Backend/src/app.js
+++ practice-7/Backend/src/app.js
@@ -1,30 +1,44 @@
-
-
 const express = require("express");
 const app = express();
-const userModel = require("./models/user.model")
+const userModel = require("./models/user.model");
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/api/user",async(req,res)=>{
+app.post("/api/user", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await userModel.create({ name, email, password });
 
-    const {name, email, password} = req.body;
-   const user = await userModel.create({name,email,password})
+  res.status(201).json({
+    message: "User created successfully...",
+    user,
+  });
+});
 
-    res.status(201).json({
-        message:"User created successfully...",
-        user
-    })
-})
+app.get("/api/user", async (req, res) => {
+  const user = await userModel.find();
+  res.status(200).json({
+    message: "User fetched successfully..",
+    user,
+  });
+});
 
-app.get("/api/user",async(req,res)=>{
-    
-   const user = await userModel.find()
+app.delete("/api/user/:id", async (req, res) => {
+
+    const id = req.params.id;
+
+    await userModel.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "User deleted",
+  });
+});
+
+app.patch("/api/user/:id",async(req,res)=>{
+    const id = req.params.id;
+  const {name,email}= req.body;
+    await userModel.findByIdAndUpdate(id,{name,email})
     res.status(200).json({
-        message:"User fetched successfully..",
-        user
+        message:"User updated successfully"
     })
 })
-
 
 module.exports = app;
