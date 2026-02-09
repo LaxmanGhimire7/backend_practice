b3c8fba4d03f0cec8c31e6f92cf3efd7c9af1ebf
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-02-09 22:48:44 +0545

    todays

--- practice-7/Backend/src/app.js
+++ practice-7/Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const userModel = require("./models/user.model");
+const cors = require("cors")
 
 app.use(express.json());
+app.use(cors())
 
 app.post("/api/user", async (req, res) => {
   const { name, email, password } = req.body;
