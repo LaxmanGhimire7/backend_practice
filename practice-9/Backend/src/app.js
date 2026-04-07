3219fed1b47e7e7203d6bb4ced631d921989c7a1
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-04-07 22:10:55 +0545

    today practice

--- practice-9/Backend/src/app.js
+++ practice-9/Backend/src/app.js
@@ -2,9 +2,27 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 
+const productModel = require("./Model/product.model")
 
 
+app.post("/api/product",async(req,res)=>{
+    const {productName, color, price} = req.body;
+    const product = await productModel.create({productName, color, price}) 
+    console.log(product)
+    res.status(201).json({
+        message:"Product create successfully..",
+        product
+    })
+})
 
+
+app.get("/api/product",async(req,res)=>{
+    const product = await productModel.find();
+    res.status(200).json({
+        message:"Product fetched successffully",
+        product
+    })
+})
 
 
 
