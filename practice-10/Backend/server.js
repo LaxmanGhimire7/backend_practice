06a2b98ce26878793c6df2d1d672b6330f8eafb0
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2026-04-16 22:23:09 +0545

    practice

--- practice-10/Backend/server.js
+++ practice-10/Backend/server.js
@@ -2,3 +2,10 @@
   * server lai start garne listen garne server.js bata
   * 
  */
+ const app = require("./src/app");
+
+
+
+ app.listen(3000,()=>{
+    console.log("Server is running on port 3000...")
+ })
--- practice-10/Backend/src/app.js
+++ practice-10/Backend/src/app.js
@@ -2,3 +2,13 @@
  * server lai config garne ho app.js ma
  * api creation
  */
+
+const express = require("express");
+const app = express()
+
+
+
+
+
+
+module.exports = app;
